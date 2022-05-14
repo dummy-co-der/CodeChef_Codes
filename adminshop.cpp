0c71c282b5a59354cf27177560cfaf387988be4e
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-14 23:45:12 +0530

    start35b

---
+++ adminshop.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<conio.h>
+#include<algorithm>
+#include<vector>
+#define ll long long
+using namespace std;
+ 
+int main() 
+{
+    int a;
+    cin>>a;
+    while(a--)
+{
+       double n,x;
+       cin>>n>>x;
+       double sum = 0;
+       vector<int> v(n);
+    for(int i=0; i<n ; i++)
+{
+      cin>>v[i];
+    }
+    if(n==1)
+{
+      cout<<ceil(x/v[0])<<endl;
+    }
+    else
+{
+    sort(v.begin(),v.end());
+    ll k = ceil(x/v[0]);
+    if(k>n)
+{
+        cout<<k<<endl;;
+    }
+    else cout<<n<<endl;
+}
+}
+return 0;
+}
---
+++ darlig.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+ 
+int main() 
+{
+    int a;
+    cin>>a;
+    while(a--)
+  {
+       int n,b;
+       cin>>n>>b;
+    if(n%4==1||n%4==2||n%4==3)
+    {
+        if(b==1)
+        {
+            cout<<"Ambiguous \n";
+        }
+        else cout<<"On \n"<<endl;
+    }
+     else
+   {
+         if(b==0)
+       {
+         cout<<"Off \n";
+    
+        }
+     else 
+cout<<"On \n";
+    }
+ }
+    return 0;
+}
+
