683037a456f53466be91bf33aaeba5924f7b2b53
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-14 23:36:44 +0530

    start36b

---
+++ presents.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    while(a--)
+    {
+        int z,b,c,ans;
+        cin>>z;
+        c=z/5;
+        b=z%5;
+        ans =4*c+b;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
