009c5132762e8f745a7f11096b2b6ba88d4de070
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-16 11:14:10 +0530

    LTIME108C

---
+++ todolist.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	while(a--)
+	{
+	    int z,c=0;
+	    cin>>z;
+	    for (int i=0; i<z; i++)
+	    {
+	        int b;
+	        cin>>b;
+	        if(b>=1000)
+	        c++;
+	    }
+	    cout<<c<<endl;
+	}
+	return 0;
+}
