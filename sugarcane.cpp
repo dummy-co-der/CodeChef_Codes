6b4cf30ae63b5644b03b2ad2a0f44c352dd8b613
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-17 19:42:31 +0530

    MAY221C

---
+++ sugarcane.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	while(a--)
+	{
+	    int n,t;
+	    cin>>n;
+	    t=50*n;
+	    t=0.3*t;
+	    cout<<t<<endl;
+	}
+	return 0;
+}
