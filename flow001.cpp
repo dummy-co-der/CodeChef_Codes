449098a38d9bd37a53af125270300aacc1b1319f
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-17 20:02:11 +0530

    flow001

---
+++ flow001.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+   int z;
+   cin>>z;
+   int a,b;
+   while(z--)
+   {
+       int sum=0;
+       cin>>a>>b;
+       sum=a+b;
+       cout<<sum<<"\n";
+   }
+   return 0;
+}
