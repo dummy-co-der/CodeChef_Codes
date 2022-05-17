f07cbbd13bfe095cf641f1ce876b614efe60fd98
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-17 19:44:04 +0530

    MAY221C

---
+++ queenattack.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int z;
+	cin>>z;
+	while(z--)
+	{
+	    int a,x,y,t,b,c,d,e;
+	    cin>>a>>x>>y;
+	    t=a-x+x-1+a-y+y-1;
+	    b=min(x-1,y-1);
+	    d=min(x-1,a-y);
+	    c=min(a-x,y-1);
+	    e=min(a-x,a-y);
+	    t+=b+d+c+e;
+	    cout<<t<<endl;
+	}
+	return 0;
+}
