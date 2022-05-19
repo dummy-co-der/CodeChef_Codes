3fa093ffed7cb9696dd8a920bbf013267367c1df
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-19 13:59:41 +0530

    start39d

---
+++ c_rating.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int c = (b-a)/8;
+	    if((b-a)%8!=0)
+	    {
+	        cout<<c+1<<endl;
+	    }
+	    else cout<<c<<endl;
+	    
+	}
+	    return 0;
+}
