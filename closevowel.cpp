17452a6a7b3cf1893c6ed35adc88161534dd2b42
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-19 13:57:29 +0530

    start39d

---
+++ closevowel.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+	int code;
+	cin>>code;
+	set<char> a;
+	a.insert('c');
+	a.insert('g');
+	a.insert('l');
+	a.insert('r');
+	while(code--)
+	{
+	    int l, ans=1;
+	    string t;
+	    cin>>l>>t;
+	    for(char ch:t)
+	    if(a.find(ch)!=a.end())
+	    ans=(ans*2)%1000000007;
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
