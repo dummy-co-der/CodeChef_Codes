9affc209413d8a3273bfc50b849bef6257beaf64
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-23 23:21:12 +0530

    MAY222D

---
+++ practiceperf.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[4];
+    for ( int i=0; i<4;i++)
+    cin>>arr[i];
+    int c=0;
+    for ( int i=0; i<4;i++)
+    { 
+        if (arr[i]>=10)
+        c++;
+    }
+    cout<<c<<endl;
+    return 0;
+}
