c7b458ba001cfb07fd37229f4fa026b3a848491a
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-23 23:17:45 +0530

    MAY222D

---
+++ highscore.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+    int g; 
+    cin>>g;
+    while(g--)
+    {
+        int v,w,x,y,answer,z;
+        cin>>v>>w>>x>>y>>z;
+        answer =max(w, max(x, max ( y,z)));
+        cout<<answer<<endl;
+    }
+    return 0;
+}
