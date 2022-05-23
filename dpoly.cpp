cf322959178264b20951c4d70fc74582e8627f2f
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-05-23 23:14:08 +0530

    MAY222D

---
+++ dpoly.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+    {
+        int z;
+        cin>>z;
+      while(z--)
+        {
+            int a, answer;
+            cin>>a;
+            int m[a];
+            for( int i=0; i<a; i++)
+            {
+                cin>>m[i];
+                if( m[i] !=0)
+                answer =i; 
+            }
+            cout<<answer<<endl;
+        }
+        return 0;
+    }
