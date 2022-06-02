feb1dcda8dc27577b463328fb7cb45f3157c4c89
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-06-02 22:53:02 +0530

    start41d

---
+++ increar.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+int i;
+cin>>i;
+while(i--)
+{
+int m,n;
+cin>>m>>n;
+if(m>n){
+int opr=m-n;
+if(opr%2==0){
+cout<<opr/2<<endl;
+}
+else{cout<<(opr/2)+2<<endl;
+}}
+else if(n>m)
+{cout<<n-m<<endl;}
+else 
+cout<<"0"<<endl;
+}
+return 0;
+}
+
+
