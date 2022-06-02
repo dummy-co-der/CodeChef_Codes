e514e8ebac8f0fb57589c313964bf517a6fc7c56
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-06-02 22:52:04 +0530

    start41d

---
+++ ispar.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+bool odd(int t)
+{
+    return t&1;
+}
+int main() 
+{
+	ll i;
+	cin>>i;
+	while (i--)
+	{
+	    ll x;
+	    ll y;
+	    cin>>x>>y;
+	    if(y==1)
+	    {
+	        if(odd(x))
+	        cout<<"odd"<<"\n";
+	        else
+	        cout<<"even"<<"\n";
+	    }
+	    else if(y==2)
+	        cout<<"odd"<<"\n";
+	    else
+	    cout<<"even"<<"\n";
+	}
+	return 0;
+}
