a8f3ce183fdb697a2f26210861d73e45388609c5
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-06-06 12:27:33 +0530

    cook142d

---
+++ songs.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int loop,m,ans;
+	    cin>>ans>>m;
+	    loop=m*3;
+	    cout<<ans/loop;
+	    cout<<endl;
+	}
+	return 0;
+}
