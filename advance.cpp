1af91f57ce79c9b921ad9c29fa7f466f33963d13
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-06-06 12:28:16 +0530

    cook142d

---
+++ advance.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(y>=x && y<=x+200)
+	    cout<<"yes"<<"\n";
+	   // else if (y<=x+200)
+	   // cout<<"yes"<<"\n";
+	    else
+	    cout<<"no"<<"\n";
+	}
+	return 0;
+}
