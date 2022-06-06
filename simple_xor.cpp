0587771d846a1cf09dd158f8d7112105cf0e0c68
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-06-06 12:30:19 +0530

    cook142d

---
+++ simple_xor.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int girl,boy;
+	    cin>>girl>>boy;
+	    int flag=0;
+	    int count=0;
+	    for(int i=girl; i<=boy; i++)
+	    {
+	        int m=i;
+	        int n=i+1;
+	        int o=i+2;
+	        int p=i+3;
+	        if((m^n^o^p)==0 && p<=boy){
+	            flag=1;
+	            cout<<m<<" "<<n<<" "<<o<<" "<<p<<endl;
+	            break;
+	    }
+	        else{
+	            count++;
+	            if(count>10)
+	            break;
+	    }}
+	    if(flag==0)
+	        cout<<-1<<endl;
+	}
+	return 0;
+}
