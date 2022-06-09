3f293ce0044e59573d5272a8ad8409e1ecf82739
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-06-09 15:10:18 +0530

    start40d

---
+++ whatsapp.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // your code goes here
+    int d;
+    cin >> d;
+    while (d--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << (a - b) * c << endl;
+    }
+    return 0;
+}
