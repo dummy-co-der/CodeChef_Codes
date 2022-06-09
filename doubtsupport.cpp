728cc078f2914b2c84d2606d17fb8d57c393e6b4
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-06-09 15:09:41 +0530

    start40d

---
+++ doubtsupport.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // your code goes here
+    int d;
+    cin >> d;
+    if (d = 800 && d <= 1600)
+    {
+        cout << "yes";
+    }
+    else
+        cout << "no";
+
+    return 0;
+}
