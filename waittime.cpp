8eea7dba3f300d5bf0be2f6e74e6420b83700ea0
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-06-16 00:14:42 +0530

    start43d

---
+++ waittime.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // your code goes here
+   
+    int num;
+    cin >> num;
+    while (num--)
+    {
+        int num1, num2;
+        cin >> num1 >> num2;
+        cout << num1 * 7 - num2 << "\n";
+    }
+    return 0;
+}
